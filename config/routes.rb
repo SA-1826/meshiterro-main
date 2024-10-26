fdf7abc53dee6ba9b11c7f3b87840585abb07721
EC2 Default User <177551857+SA-1826@users.noreply.github.com>
2024-10-26 06:26:38 +0000

    [Update] 管理者機能実装－ログイン・ログアウト後の遷移先指定まで

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: :about
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+end
